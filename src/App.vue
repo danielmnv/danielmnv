8558eaaeb26740b9c334960c8a8842c72d5d812a
danielmnv <danmnvx@gmail.com>
2021-01-21 20:55:37 -0600

    initla commit for resumee

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <v-app>
+    <p class="text-h6 text-center">Resumee danielmnv</p>
+  </v-app>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+
+export default Vue.extend({
+  name: 'App',
+});
+</script>

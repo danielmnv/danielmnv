385567ffe8b380d72b920c3463910651770fef29
danielmnv <danmnvx@gmail.com>
2021-01-25 21:38:54 -0600

    Basic config for theme dark

--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -1,7 +1,17 @@
 import Vue from 'vue';
 import Vuetify from 'vuetify/lib/framework';
+import colors from 'vuetify/lib/util/colors';
 
 Vue.use(Vuetify);
 
 export default new Vuetify({
+    theme: {
+        dark: true,
+        themes: {
+            dark: {
+                primary: colors.cyan.base,
+                background: colors.grey.darken4
+            }
+        }
+    }
 });

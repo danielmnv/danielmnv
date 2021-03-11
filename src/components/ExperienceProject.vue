ac65be4d2c806ea67ad140859bfe0c0e4cae3063
danielmnv <danmnvx@gmail.com>
2021-03-10 23:14:07 -0600

    Project card, it shows an image, languages and desc

--- src/components/ExperienceProject.vue
+++ src/components/ExperienceProject.vue
@@ -1,78 +1,66 @@
 <template>
     <v-hover v-slot="{ hover }">
-        <v-card outlined tile class="mx-2 project-card elevation-2">
+        <v-card :ref="project.name" rounded="lg" class="mx-2 elevation-2">
+            <!-- Background -->
             <v-img
-                class="blurr-img"
-                src="@/assets/experience/dashboard.png"
+                :class="isMdScreen ? 'align-center justify-center' : 'rounded-lg'"
+                :src="require('@/assets/experience/' + project.img)"
+                :lazy-src="require('@/assets/experience/' + project.img)"
             >
+                <!-- Button to represent that the user can touch the card and the content will appear (only phones & tablets) -->
+                <v-btn 
+                    fab
+                    x-small 
+                    color="accent"
+                    v-show="isMdScreen && !hover"
+                >
+                    <v-icon>mdi-magnify</v-icon>
+                </v-btn>
             </v-img>
-            <!-- Languages / Frameworks -->
-            <template v-if="!mobile">
-                <v-fade-transition origin="top center 0">
-                    <v-overlay
-                        v-if="hover"
-                        absolute
-                        color="accent"
-                        :opacity="isThemeDark ? 0.9 : 0.8"
-                    >
-                        <v-container>
-                            <v-card-title>{{ project.name }}</v-card-title>
 
-                            <v-card-text>
-                                <p v-for="(paragraph, index) in project.text" :key="index">
-                                    {{ paragraph }}
-                                </p>
-                            </v-card-text>
-                            <!-- <v-list dense color="transparent">
-                                <v-list-item
-                                    v-for="tool in project.tools"
-                                    :key="tool.name"
-                                >
-                                    <v-list-item-avatar
-                                        :color="tool.color"
-                                        size="35"
-                                    >
-                                        <template v-if="tool.name == 'Prestashop'">
-                                            <img :src="require('@/assets/icons/' + tool.avatar)" :alt="tool.name"/>
-                                        </template>
-                                        <template v-else>
-                                            <v-icon v-text="tool.icon"></v-icon>
-                                        </template>
-                                    </v-list-item-avatar>
-                                    <v-list-item-content>
-                                        <v-list-item-title
-                                            v-text="tool.name"
-                                        ></v-list-item-title>
-                                    </v-list-item-content>
-                                </v-list-item>
-                            </v-list> -->
-                        </v-container>
-                    </v-overlay>
-                </v-fade-transition>
-            </template>
+            <!-- Title for phones & tablets -->
+            <v-card-title v-if="isMdScreen">{{ project.name }}</v-card-title>
+            
+            <!-- Content -->
+            <v-fade-transition origin="top center 0">
+                <v-overlay
+                    absolute
+                    color="accent"
+                    v-if="hover"
+                    :opacity="isThemeDark ? 0.9 : 0.8"
+                >
+                    <v-card-text>
+                        <!-- Title for desktop -->
+                        <span class="text-h6" v-if="!isMdScreen">{{ project.name }}</span>
 
-            <template v-else>
-                <div class="d-flex flex-wrap justify-center pb-3">
-                    <v-chip
-                        v-for="(tool, index) in project.tools"
-                        :key="index"
-                        :color="tool.color"
-                        class="ma-1"
-                        small
-                        outlined
-                    >
-                        <template v-if="tool.name == 'Prestashop'">
-                            <v-avatar left>
-                                <img :src="require('@/assets/icons/' + tool.avatar)" :alt="tool.name" />
-                            </v-avatar>
-                        </template>
-                        <template v-else>
-                            <v-icon dark left>{{ tool.icon }}</v-icon>
-                        </template>
-                        {{ tool.name }}
-                    </v-chip>
-                </div>
-            </template>
+                        <!-- Short description -->
+                        <p class="text-body-2 my-6">{{ project.text }}</p>
+
+                        <!-- Languages / Frameworks -->
+                        <div class="d-flex flex-wrap justify-center pb-3">
+                            <v-chip
+                                v-for="(tool, index) in project.tools"
+                                :key="index"
+                                :color="tool.color"
+                                :small="isMdScreen"
+                                class="ma-1"
+                                label
+                            >
+                                <!-- Icon/avatar -->
+                                <template v-if="tool.name == 'Prestashop'">
+                                    <v-avatar left>
+                                        <img :src="require('@/assets/icons/' + tool.avatar)" :alt="tool.name" />
+                                    </v-avatar>
+                                </template>
+                                <template v-else>
+                                    <v-icon dark left >{{ tool.icon }}</v-icon>
+                                </template>
+                                {{ tool.name }}
+                            </v-chip>
+                        </div>
+                    </v-card-text>
+                </v-overlay>
+            </v-fade-transition>
         </v-card>
     </v-hover>
 </template>
@@ -85,24 +73,14 @@ export default class ExperienceProject extends Vue {
     @Prop({ required: true }) readonly project!: object;
     @Prop({ default: true }) readonly width!: boolean;
 
-     // Is mobile or not
-    get mobile(): boolean {
-        return this.$vuetify.breakpoint.smAndDown;
+     // Is middle screen or not
+    get isMdScreen(): boolean {
+        return this.$vuetify.breakpoint.mdAndDown;
     }
 
     get isThemeDark(): boolean {
 		return this.$vuetify.theme.dark;
 	}
+
 }
 </script>
-
-<style scoped>
-.project-card {
-    border-radius: 12px !important;
-}
-
-.blurr-img {
-    filter: blur(1px);
-    -webkit-filter: blur(1px);
-}
-</style>

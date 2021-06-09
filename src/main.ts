71748d7116721f1dd73c0630d577ab39e3da856b
danielmnv <danmnvx@gmail.com>
2021-06-09 15:25:29 -0500

    Firebase config

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,24 @@ import App from './App.vue'
 import router from './router'
 import vuetify from './plugins/vuetify';
 import * as VueGoogleMaps from 'vue2-google-maps';
+import firebase from 'firebase/app';
+import 'firebase/analytics'
 
 Vue.config.productionTip = false;
 
 Vue.use(VueGoogleMaps, { load: { key: process.env.VUE_APP_MAPS_API }});
+
+firebase.initializeApp({
+  apiKey: process.env.VUE_APP_FB_API_KEY,
+  authDomain: process.env.VUE_APP_FB_AUTH_DOMAIN,
+  projectId: process.env.VUE_APP_FB_PROJECT_ID,
+  storageBucket: process.env.VUE_APP_FB_STORAGE_BUCKET,
+  messagingSenderId: process.env.VUE_APP_FB_MESSAGING_SENDER_ID,
+  appId: process.env.VUE_APP_FB_APP_ID,
+  measurementId: process.env.VUE_APP_FB_MEASUREMENT_ID
+});
+
+Vue.prototype.$analytics = firebase.analytics();
 
 new Vue({
   router,

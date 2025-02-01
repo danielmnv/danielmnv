7ef12d9e4d0db631d4e979036ad1fbed7f5f9379
Daniel Molina <danmnvx@gmail.com>
2025-01-31 21:15:10 -0600

    Migrate from Vue to Next/React

---
+++ src/app/page.tsx
@@ -0,0 +1,26 @@
+export default function Home() {
+  return (
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+        <svg
+          version="1.1"
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 300 197"
+          width="300"
+          height="197"
+        >
+          <path
+            d="M0,0 L6,0 L11,4 L18,14 L31,32 L41,46 L45,41 L55,27 L70,5 L74,2 L81,2 L87,7 L104,76 L122,147 L125,159 L125,169 L121,174 L13,174 L-16,175 L-30,175 L-40,172 L-43,167 L-41,156 L-20,69 L-5,6 L-2,1 Z "
+            fill="white"
+            transform="translate(169,8)"
+          />
+          <path
+            d="M0,0 L20,0 L41,4 L58,10 L72,17 L88,28 L99,40 L103,45 L112,61 L116,74 L117,84 L117,97 L113,113 L105,129 L98,138 L82,153 L64,163 L50,169 L37,173 L19,177 L2,177 L-3,174 L-5,170 L-4,118 L-5,57 L-5,6 Z "
+            fill="white"
+            transform="translate(16,7)"
+          />
+        </svg>
+      </main>
+    </div>
+  );
+}

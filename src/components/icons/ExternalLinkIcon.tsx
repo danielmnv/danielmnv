e5c46559e9f90812a1a9574f97cd7e95a8af6dd1
Daniel Molina <danmnvx@gmail.com>
2025-06-17 21:26:23 -0600

    Added icons and social links

---
+++ src/components/icons/ExternalLinkIcon.tsx
@@ -0,0 +1,17 @@
+export function ExternalLinkIcon() {
+  return (
+    <svg
+      className="absolute right-6 top-6 h-5 w-5 text-neutral-200 transition-all duration-300 group-hover:-translate-y-1 group-hover:translate-x-1 group-hover:text-primary"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+    >
+      <path
+        d="M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
+}

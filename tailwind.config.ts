627761078e255d92d7a667d83ca2ef76593db94d
Daniel Molina <danmnvx@gmail.com>
2025-06-17 21:21:27 -0600

    Setup config and intial files

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,7 @@
 import type { Config } from "tailwindcss";
 
-export default {
+const config: Config = {
+  darkMode: 'class',
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
@@ -9,10 +10,26 @@ export default {
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        primary: 'rgb(var(--color-primary) / 1)',
+        secondary: 'rgb(var(--color-secondary) / 1)',
+        accent: 'rgb(var(--color-accent) / 1)',
+        background: 'rgb(var(--color-background) / 1)',
+        foreground: 'rgb(var(--color-foreground) / 1)',
+        neutral: {
+          50: 'rgb(var(--color-neutral-50) / 1)',
+          100: 'rgb(var(--color-neutral-100) / 1)',
+          200: 'rgb(var(--color-neutral-200) / 1)',
+          300: 'rgb(var(--color-neutral-300) / 1)',
+          400: 'rgb(var(--color-neutral-400) / 1)',
+          500: 'rgb(var(--color-neutral-500) / 1)',
+        }
+      },
+      fontFamily: {
+        sans: ['Inter', 'sans-serif'],
       },
     },
   },
   plugins: [],
-} satisfies Config;
+};
+
+export default config; 
